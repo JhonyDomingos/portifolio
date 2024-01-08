358f13afce3f38a440626089b7f318f7dbf6175b
JhonyDomingos <jhonatandomingossilva@oulook.com.br>
2024-01-08 18:04:00 -0300

    Atualizando repo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
   return (
     <header>
-        <p>header</p>
+        
     </header>
   )
 };
---
+++ src/Pages/TechAndSkills/TechAndSkills.jsx
@@ -0,0 +1,7 @@
+
+
+export const TechAndSkills = () => {
+  return (
+    <div>TechAndSkills</div>
+  )
+}
